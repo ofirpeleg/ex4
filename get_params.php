a3045639986f5897bce58816bca048409b3f6f9a
ofirpeleg <ofirpeleg2111@gmail.com>
2021-04-27 12:17:08 +0300

    commit

---
+++ get_params.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+  <head>
+    <title>Form 2</title>
+  </head>
+  <body>
+        <?php
+
+        $item = $_GET["item"];
+        $color = $_GET["color"];
+
+            echo "<h3>Hello, the item you added is  " . $item .  "</h3>";
+        ?>
+
+  </body>
+</html>
